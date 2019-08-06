bed10b69dd12a28810700b842513bd6550c60c33
ComLarsic <53827950+ComLarsic@users.noreply.github.com>
2019-08-06 19:20:43 +0200

    Uninstall script

---
+++ Uninstall_Linux.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3.6
+import os
+
+usr = input("Type the current username of the linux user: ")
+
+os.system("rm /usr/share/applications/Velvet.desktop")
+os.system(f"rm -rf /home/{usr}/Velvet/")
