c57392ce12964cdce543c8df014f2e50b26e9fea
ComLarsic <53827950+ComLarsic@users.noreply.github.com>
2019-08-18 11:00:22 +0200

    Added features
    - Tidied up the code
    - Address bar now automatically fills, "https//:" to the the beginning of the url
    - Vaulted the history button, now only accessible by putting "about:history" in the address bar

--- main.py
+++ main.py
@@ -7,84 +7,98 @@ gi.require_version("Gtk", "3.0")
 gi.require_version("WebKit2", "4.0")
 from gi.repository import Gtk, WebKit2
 
-#Defining variables
-dir = os.path.dirname(os.path.abspath(__file__))
-usr = getpass.getuser()
-window = Gtk.Window()
-headbar = Gtk.HeaderBar()
-webview = WebKit2.WebView()
-window.set_title("Velvet Browser")
-scrolled_window = Gtk.ScrolledWindow()
-entry = Gtk.Entry()
-headbar.set_custom_title(entry)
+class Velvet():
+      
+        def __init__(self):
+                #Defining variables
+                self.window = Gtk.Window()
+                self.headbar = Gtk.HeaderBar()
+                self.webview = WebKit2.WebView()
+                self.window.set_title("Velvet Browser")
+                self.window.set_icon_from_file('icon.png')
+                self.scrolled_window = Gtk.ScrolledWindow()
+                self.directory = os.path.dirname(os.path.abspath(__file__))
+                self.url = self.webview.get_uri()
+                self.entry = Gtk.Entry()
+                self.headbar.set_custom_title(self.entry)
+                self.go_back_button = Gtk.Button()
+                self.go_back_arrow = Gtk.Image.new_from_icon_name("go-previous", Gtk.IconSize.SMALL_TOOLBAR)
+                self.go_back_button.add(self.go_back_arrow)
+                self.go_back_button.connect("clicked", self.on_go_back)
+                self.go_forward_button = Gtk.Button()
+                self.go_forward_arrow = Gtk.Image.new_from_icon_name("go-next", Gtk.IconSize.SMALL_TOOLBAR)
+                self.go_forward_button.add(self.go_forward_arrow)
+                self.go_forward_button.connect("clicked", self.on_go_forward)
+                self.entry.connect("activate", self.on_enter)
+                self.entry.set_size_request(500,50)
 
-#Back/Forward buttons
+                
 
-def on_go_back(self):
-    webview.go_back()
+                self.reload_button = Gtk.Button()
+                self.reload_icon = Gtk.Image.new_from_icon_name("object-rotate-left", Gtk.IconSize.SMALL_TOOLBAR)
+                self.reload_button.add(self.reload_icon)
+                self.reload_button.connect("clicked", self.on_reload)
 
-def on_go_forward(self):
-    webview.go_forward()
+                #Buttons
+                self.headbar.pack_start(self.go_back_button)
+                self.headbar.pack_start(self.go_forward_button)
+                self.headbar.pack_start(self.reload_button)
+ 
+                #window
+                self.scrolled_window.add(self.webview)
+                self.headbar.set_show_close_button(True)
+                self.window.set_titlebar(self.headbar)
+                self.window.set_default_size(1000,600)
+                self.window.add(self.scrolled_window)
+                #webview
 
-go_back_button = Gtk.Button()
-go_back_arrow = Gtk.Image.new_from_icon_name("go-previous", Gtk.IconSize.SMALL_TOOLBAR)
-go_back_button.add(go_back_arrow)
-go_back_button.connect("clicked", on_go_back)
+                self.webview.load_uri("https://duckduckgo.com")
+                url = self.webview.get_uri()
+                self.entry.set_text(url)
+                #window
+                self.window.connect("destroy", self.on_destroy)
+                self.window.show_all()
+                Gtk.main()
+        
+        #Back/Forward buttons
+        def on_history(self, webview):
+                directory = os.path.dirname(os.path.abspath(__file__))
+                self.webview.load_uri(f"file:///{directory}/history.html")
 
-go_forward_button = Gtk.Button()
-go_forward_arrow = Gtk.Image.new_from_icon_name("go-next", Gtk.IconSize.SMALL_TOOLBAR)
-go_forward_button.add(go_forward_arrow)
-go_forward_button.connect("clicked", on_go_forward)
+        def on_go_back(self, webview):
+                self.webview.go_back()
+                url = self.webview.get_uri()
+                self.entry.set_text(url)
+
+        def on_go_forward(self, webview):
+                self.webview.go_forward()
+                url = self.webview.get_uri()
+                self.entry.set_text(url)
+
+        def on_reload(self, webview):
+                self.webview.reload()
+                url = self.webview.get_uri()
+                self.entry.set_text(url)
+
+        def on_destroy(self, window):
+                Gtk.main_quit()
+
+        def on_enter(self, entry):
+                entry_uri = self.entry.get_text()
+                url = self.webview.get_uri()
+                directory = os.path.dirname(os.path.abspath(__file__))
+                print(url)
+                with open(f"{directory}/history.html", 'a') as history:
+                        history.write("* <a href=\"" + url + "\">" + url + "</a><br>")
+                if entry_uri.startswith('http://') or entry_uri.startswith('https://'):
+                        self.webview.load_uri(entry_uri)
+                elif (entry_uri == "about:history"):
+                        self.webview.load_uri(f"file:///{directory}/history.html")
+                        return
+                else:
+                        entry_uri = f'https://{entry_uri}'
+                        self.entry.set_text(entry_uri)
+                        self.webview.load_uri(entry_uri)
 
 
-
-def on_reload(self):
-        webview.reload()
-
-def on_history(self):
-        webview.load_uri(f"file:///{dir}/history.html")
-
-def on_destroy(window):
-    Gtk.main_quit()
-
-def on_enter(entry):
-    url = webview.get_uri()
-    entry_uri = entry.get_text()
-    print(url)
-    webview.load_uri(f"https://{entry_uri}")
-    with open(f"{dir}/history.html", 'a') as history:
-                history.write("* <a href=\"" + url + "\">" + url + "</a><br>")
-    if (entry_uri == "about:history"):
-        webview.load_uri(f"file:///{dir}/history.html")
-        return
-
-entry.connect("activate", on_enter)
-
-history_button = Gtk.Button()
-history_icon = Gtk.Image.new_from_icon_name("weather-fog", Gtk.IconSize.SMALL_TOOLBAR)
-history_button.add(history_icon)
-history_button.connect("clicked", on_history)
-
-reload_button = Gtk.Button()
-reload_icon = Gtk.Image.new_from_icon_name("object-rotate-left", Gtk.IconSize.SMALL_TOOLBAR)
-reload_button.add(reload_icon)
-reload_button.connect("clicked", on_reload)
-
-#Buttons
-headbar.pack_start(go_back_button)
-headbar.pack_start(go_forward_button)
-headbar.pack_start(reload_button)
-headbar.pack_start(history_button)
-#window
-scrolled_window.add(webview)
-headbar.set_show_close_button(True)
-window.set_titlebar(headbar)
-window.set_default_size(1000,600)
-window.add(scrolled_window)
-#webview
-
-webview.load_uri("https://www.duckduckgo.com")
-#window
-window.connect("destroy", on_destroy)
-window.show_all()
-Gtk.main()
+run = Velvet()
